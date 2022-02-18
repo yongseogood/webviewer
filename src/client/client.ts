a86dae5ffa9d94ef2753cbad0c0a58b089b473fb
Yong Won Seo <yseo@velodyne.com>
2022-02-18 05:48:55 -0800

    reading pcd

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import Stats from 'three/examples/jsm/libs/stats.module'
+import { PCDLoader } from 'three/examples/jsm/loaders/PCDLoader'
 import { GUI } from 'dat.gui'
 
 const scene = new THREE.Scene()
@@ -20,44 +21,58 @@ document.body.appendChild(renderer.domElement)
 
 new OrbitControls(camera, renderer.domElement)
 
-const loader = new THREE.FileLoader()
+// const loader = new THREE.FileLoader()
+const loader = new PCDLoader()
 
 const streamGeometry = new THREE.BufferGeometry()
 
-const frameSize = 2000
+// const frameSize = 2000
+const frameSize = 17
 for(let t = 0; t < frameSize; t++)
 {
-    let filename = "/pcl/point_00001_0000" + String(t) + ".txt"
+    // let filename = "/pcl/point_00001_0000" + String(t) + ".txt"
+
+    // if(t >= 10 && t < 100)
+    //     filename = "/pcl/point_00001_000" + String(t) + ".txt"
+    // else if(t >= 100 && t < 1000)
+    //     filename = "/pcl/point_00001_00" + String(t) + ".txt"
+    // else if(t >= 1000 && t < 10000)
+    //     filename = "/pcl/point_00001_0" + String(t) + ".txt"
+
+    let filename = "/pcd/0000" + String(t) + ".pcd"
 
     if(t >= 10 && t < 100)
-        filename = "/pcl/point_00001_000" + String(t) + ".txt"
-    else if(t >= 100 && t < 1000)
-        filename = "/pcl/point_00001_00" + String(t) + ".txt"
-    else if(t >= 1000 && t < 10000)
-        filename = "/pcl/point_00001_0" + String(t) + ".txt"
+        filename = "/pcd/000" + String(t) + ".pcd"
 
     console.log("logname = " + filename)
+    // loader.load(filename,
+    //     // onLoad callback
+    //     function ( data ) {
+    //         // output the text to the console
+    //         console.log('testing yong')
+    //         const lines = data.toString().split("\n")
+
+    //         const points = []
+
+    //         for(let i = 1; i < lines.length; i++)
+    //         {
+    //             const pos = lines[i].toString().split(' ')
+
+    //             points.push(new THREE.Vector3(Number(pos[0]), Number(pos[1]), Number(pos[2])))
+    //         }
+
+    //         streamGeometry.setFromPoints(points)
+
+    //         const geometry = new THREE.BufferGeometry().setFromPoints(points)
+    //         const point = new THREE.Points(geometry, new THREE.PointsMaterial({color: 0xffffff, size:0.1}))
+    //         // scene.add(point)
+    //     }
+    // );
+
     loader.load(filename,
-        // onLoad callback
-        function ( data ) {
-            // output the text to the console
-            console.log('testing yong')
-            const lines = data.toString().split("\n")
+        function (points) {
 
-            const points = []
-
-            for(let i = 1; i < lines.length; i++)
-            {
-                const pos = lines[i].toString().split(' ')
-
-                points.push(new THREE.Vector3(Number(pos[0]), Number(pos[1]), Number(pos[2])))
-            }
-
-            streamGeometry.setFromPoints(points)
-
-            const geometry = new THREE.BufferGeometry().setFromPoints(points)
-            const point = new THREE.Points(geometry, new THREE.PointsMaterial({color: 0xffffff, size:0.1}))
-            // scene.add(point)
+            scene.add( points );
         }
     );
 }
